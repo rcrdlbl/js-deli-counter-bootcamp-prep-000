37903a1d444891ad55f29f68b0f8841fd4ac262c
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-09 22:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty.";
   } else for (var i = 0; i < line.length; i++) {
-    status = status + `${i + 1}. ${line[i]}, `;
+    status = status + `${i + 1}. ${line[i]}`;
+    if (i != line.length) {
+      status = status + ",";
+    }
     }
   return status;
 }
