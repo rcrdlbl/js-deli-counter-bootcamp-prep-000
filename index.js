5055fce9ffb9280d8315194840e852fbc4d88012
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-09 22:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty.";
   } else for (var i = 0; i < line.length; i++) {
-    status = status + `${i + 1}. ${line[i]}`;
+    status = status + `${i + 1}. ${line[i]}, `;
     }
   return status;
 }
