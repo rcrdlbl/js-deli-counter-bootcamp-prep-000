843462c6125030a080977adffa5b4c5099a3e58c
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-09 22:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line) {
+  var status = "The line is currently: ";
   if (line.length === 0) {
     return "The line is currently empty.";
   } else for (var i = 0; i < line.length; i++) {
-    var status = "The line is currently: ";
     status = status + `${i + 1}. ${line[i]}`;
     return status;
   }
